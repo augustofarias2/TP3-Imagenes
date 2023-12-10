d66d18fb50034054f48902c4e34cafa1aa253820
GuidoLorenzetti <guidobruera@gmail.com>
2023-12-10 17:13:46 -0300

    Final 3

--- practico.py
+++ practico.py
@@ -12,7 +12,7 @@ def create_video(tirada):
     fps = int(cap.get(cv2.CAP_PROP_FPS))
 
     out = cv2.VideoWriter(f'Video-Output{tirada}.mp4', cv2.VideoWriter_fourcc(*'mp4v'), fps, (width,height))
-    flag=0
+
     while (cap.isOpened()):
         ret, frame = cap.read()
         if ret==True:
@@ -23,7 +23,6 @@ def create_video(tirada):
 
             if num_labels > 12:
                 frame=detectar_dado(frame, num_labels, labels, stats, centroids)
-                flag+=1
 
             # --- Muestro por pantalla ------------
             img_filtered_show = cv2.resize(img_filtered, dsize=(int(width/3), int(height/3)))
